2963ae7f7d96fc0f10f09abe67e987f23ef2cea9
zjy <jinyu.zheng@tenclass.com>
2021-07-21 15:38:28 +0800

    添加各种操作符以及描述

--- app/src/main/java/com/kgxl/flowlearning/FlowCenter.kt
+++ app/src/main/java/com/kgxl/flowlearning/FlowCenter.kt
@@ -1,21 +1,23 @@
 package com.kgxl.flowlearning
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
-import java.lang.Exception
 
 /**
  *  Created by zjy on 2021/7/20.
  */
-class FlowCenter {
 
+fun main(args: Array<String>) {
+    //run method start
+    onStart()
 }
 
 fun simpleDelay(): Flow<Int> = flow {
     println("start flow")
-    for (i in 1..3) {
+    for (i in 1..5) {
         delay(200)
         emit(i)
     }
@@ -26,10 +28,6 @@ fun simple(): Flow<Int> = flow {
     for (i in 1..5) {
         emit(i)
     }
-}
-
-fun main(args: Array<String>) {
-    reduceOperator()
 }
 
 /**
@@ -93,14 +91,53 @@ fun mapOperator() {
 /**
  * [Flow.flatMapConcat]操作符
  * 可以改变传入的值，但返回值必须为flow<R>
- * 与[Flow.map]相比唯一区别为返回值
+ * 与[Flow.map]相比唯一区别为返回值.[mapOperator]
+ * 与[Flow.flatMapMerge]相比它是按照输出顺序输出.[flatmapMergeOperator]
+ * 例如：
+ *  flow 1: 1,2,3
+ *  flow 2: 6,7,8
+ *  out :
+ *      1->6
+ *      1->7
+ *      1->8
+ *      2->6
+ *      2->7
+ *      2->8
+ *      3->6
+ *      3->7
+ *      3->8
+ *
  */
 fun flatmapOperator() {
     runBlocking {
         simple().flatMapConcat {
             //输出的值为flow<R>类型
             println("flatMapConcat = $it")
-            simple()
+            flowOf(1, 4).map { cur -> "$it -> $cur" }
+        }.collect {
+            println("collect = $it")
+        }
+    }
+}
+
+/**
+ * 是全部先执行完输入再执行输出
+ *  例如：
+ *  flow 1:1,2,3
+ *  flow 2:6,7,8
+ *  out :
+ *      1->6
+ *      2->7
+ *      3->8
+ *      4->9
+ *      5->10
+ */
+fun flatmapMergeOperator() {
+    runBlocking {
+        simple().flatMapMerge {
+            //输出的值为flow<R>类型
+            println("flatMapMerge = $it")
+            flowOf(4, 5).map { cur -> "$cur $it" }
         }.collect {
             println("collect = $it")
         }
@@ -155,5 +192,175 @@ fun reduceOperator() {
             a + b
         }
         println("sum = $sum")
+    }
+}
+
+/**
+ * 指定条件过滤输入值
+ */
+fun filterOperator() {
+    runBlocking {
+        simple().filter {
+            it > 3
+        }.collect {
+            println("collect = $it")
+        }
+    }
+}
+
+fun withContext() {
+
+    runBlocking {
+        flow {
+            for (i in 1..3) {
+                Thread.sleep(100)
+                emit(i)
+            }
+        }.flowOn(Dispatchers.Default).collect {
+            println("collect = $it")
+        }
+    }
+
+    runBlocking {
+        flow {
+            // The WRONG way to change context for CPU-consuming code in flow builder
+            kotlinx.coroutines.withContext(Dispatchers.Default) {
+                for (i in 1..3) {
+                    Thread.sleep(100) // pretend we are computing it in CPU-consuming way
+                    emit(i) // emit next value
+                }
+            }
+        }.collect {
+            println("collect = $it")
+        }
+    }
+}
+
+/**
+ * 在输入流时当第一个进入，缓存，直接发送，无需挨个等待
+ * 不使用buffer 顺序执行，使用buffer会先并行
+ *  结果如下：
+ *       map1 1
+ *       collect = 1
+ *       map1 2
+ *       collect = 2
+ *       map1 3
+ *       collect = 3
+ *       map1 4
+ *       collect = 4
+ *       map1 5
+ *       collect = 5
+ *
+ *       map1 buffer 1
+ *       map1 buffer 2
+ *       map1 buffer 3
+ *       map1 buffer 4
+ *       map1 buffer 5
+ *       collect buffer = 1
+ *       collect buffer = 2
+ *       collect buffer = 3
+ *       collect buffer = 4
+ *       collect buffer = 5
+ */
+fun bufferOperator() {
+    runBlocking {
+        simple().map {
+            println("map1 $it")
+            it
+        }.collect { value ->
+            println("collect = $value")
+        }
+    }
+
+    runBlocking {
+        simple().map {
+            println("map1 buffer $it")
+            it
+        }.buffer().collect { value ->
+            println("collect buffer = $value")
+        }
+    }
+}
+
+/**
+ * 取最新的
+ * 当上游的发送速度小于处理的速度。只会取最新的一个
+ */
+fun collectLatest() {
+    runBlocking {
+        simpleDelay().collectLatest {
+            println("collectLatest delay before= $it")
+            delay(250)
+            println("collectLatest delay after= $it")
+        }
+    }
+}
+
+/**
+ * 两个flow合并成一个新的flow
+ */
+fun zipOperator() {
+    runBlocking {
+        (1..5).asFlow().zip((5..10).asFlow()) { a, b ->
+            a + b
+        }.collect {
+            println("collect = $it")
+        }
+    }
+}
+
+/**
+ * catch flow中的错误作统一处理
+ * 注意此处的顺序,catch只能catch到之前操作的错误，之后的操作并不会走catch
+ */
+fun catch() {
+    runBlocking {
+        (1..5).asFlow().catch { e ->
+            println(e.message.toString())
+        }.map {
+            if (it > 3) {
+                throw IllegalStateException("number to large")
+            }
+            it
+        }.collect {
+            check(it > 3) { "number to large" }
+        }
+    }
+}
+
+/**
+ * 不管是否有异常，只要结束都会执行[onCompletion]
+ * 此方法在catch之后，一旦流有异常，此处是得不到异常的，被catch住了。
+ * 所以不要再此处处理异常.
+ */
+fun onComplete() {
+    runBlocking {
+        simple().map {
+            if (it > 3) {
+                throw IllegalStateException("number to large")
+            }
+            it
+        }.onCompletion { e ->
+            println("onCompletion : ${e?.message.toString()}")
+        }.catch { e ->
+            println("catch : ${e.message.toString()}")
+        }.collect {
+            println("collect = $it")
+        }
+    }
+}
+
+/**
+ * [Flow.onStart]在流开始的时候会执行
+ */
+fun onStart() {
+    runBlocking {
+        simple().onStart {
+            println("start")
+        }.onCompletion { e ->
+            println("onCompletion : ${e?.message.toString()}")
+        }.collect {
+            println("collect = $it")
+        }
     }
 }
